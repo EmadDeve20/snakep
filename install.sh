61ecbf1648faf2c099a818c2febf9703e1d24b7c
ED <emaddeve20@gmail.com>
2021-05-16 15:05:55 -0600

    install commonds for snakep

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ then
 fi
 cp -r .snakep ~
 
+if [ -f ~/.snakep_commonds ]
+then
+		rm -rf ~/.snakep_commonds
+fi
+cp  .snakep_commonds ~
+
+
 if [ -f /usr/bin/snakep ]
 then
 	sudo rm /usr/bin/snakep
