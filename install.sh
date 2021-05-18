4eb1fd1226c4644bd03a5ddec6b072f1c1125d8e
ED <emaddeve20@gmail.com>
2021-05-17 18:33:05 -0600

    update for all changes

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ -f ~/.snakep ]
+if [ -s ~/.snakep ]
 then
 	rm -rf ~/.snakep
 fi
 cp -r .snakep ~
 
-if [ -f ~/.snakep_commonds ]
+if [ -s ~/.snakep_commonds ]
 then
 		rm -rf ~/.snakep_commonds
 fi
 cp  .snakep_commonds ~
 
 
-if [ -f /usr/bin/snakep ]
+if [ -s /usr/bin/snakep ]
 then
 	sudo rm /usr/bin/snakep
 fi
